4b5fba42ec6338760ec2aeb44fbae917782dc80d
ShivamBunge <shivambunge9898@gmail.com>
2021-11-08 16:01:41 +0530

    done

--- calc/views.py
+++ calc/views.py
@@ -15,13 +15,14 @@ def entry(request):
     
     a=request.POST['entry']
     p=Booking.objects.filter(id=a)
-    person=Booking.objects.get(id=a)
-    print(person.parking_id)
+    
+    
     if p.exists() :
+        person=Booking.objects.get(id=a)
         d={"parking_id":person.parking_id,"vehicle_no":person.vehicle_no,"entry":person.entry}
         return render(request,"receipt.html",d)      
     else:
-        messages.info(request, "slot is already booked")
+        messages.info(request, "enter slot between 1-15")
         return render(request,"home.html")      
     
     
@@ -35,7 +36,7 @@ def exit(request):
     if p.exists() :
         print(str(datetime.datetime.now()))
         d={"parking_id":person.parking_id,"vehicle_no":person.vehicle_no,"entry":person.entry,"exit":person.exit}
-        return render(request,"receipt.html",d)      
+        return render(request,"receipt2.html",d)      
     else:
-        messages.info(request, "slot is already booked")
+        messages.info(request, "enter slot between 1-15")
         return render(request,"home.html") 
