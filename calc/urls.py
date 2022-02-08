c42f204864b4791bb2d583cbaa434af095c0a0fe
ShivamBunge <shivambunge9898@gmail.com>
2022-02-08 10:10:08 +0530

    modified

--- calc/urls.py
+++ calc/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("check_availability",views.check_availability, name="check_availability"),
     path("home", views.home, name="home"),
     path("exit", views.exit, name="exit"),  
     path("entry", views.entry )
--- calc/views.py
+++ calc/views.py
@@ -4,26 +4,29 @@ from . models import Booking
 from django.contrib import messages
 # Create your views here.
 from datetime import datetime
-
-
+import heapq as hq
+heap=[i for i in range(1,16)]
+hq.heapify(heap)
 def home(request):
     return render(request, "home.html")
 
 
 
 def entry(request):
-    
+    print(heap)
+
     a=request.POST['entry']
     p=Booking.objects.filter(parking_id=a)
-
-    if p.exists() :
+    
+    if p.exists() and int(a) in heap:
+        heap.remove(int(a))
         person=Booking.objects.get(parking_id=a)
         person.entry = datetime.now()    
         person.save()
         d={"parking_id":person.parking_id,"vehicle_no":person.vehicle_no,"entry":person.entry}
         return render(request,"receipt.html",d)      
     else:
-        messages.info(request, "enter slot between 1-15")
+        messages.info(request, "enter slot between 1-15 and check if available")
         return render(request,"home.html")      
     
     
@@ -33,12 +36,19 @@ def exit(request):
     a=request.POST['entry']
     p=Booking.objects.filter(parking_id=a)
     
-    if p.exists() :
+    if p.exists() and int(a) not in heap :
+
+        hq.heappush(heap,int(a))
         person=Booking.objects.get(parking_id=a)
         person.exit = datetime.now()    
         person.save()
         d={"parking_id":person.parking_id,"vehicle_no":person.vehicle_no,"entry":person.entry,"exit":person.exit}
         return render(request,"receipt.html",d)      
     else:
-        messages.info(request, "enter slot between 1-15")
+        messages.info(request, "Enter slot between 1-15 which is not booked")
         return render(request,"home.html") 
+
+def check_availability(request):
+    num=heap[0]
+    print(num)
+    return render(request,"home.html",{"n":num})
